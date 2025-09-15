e95ef5536f454983218e8c747cf53bb0c9cb5da9
Val <60014136+voffee@users.noreply.github.com>
2025-09-16 01:05:57 +0800

    Accordion JS functionality completed.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 
+// Declare Variables
+const icon1 = document.querySelector('.icon-1')
+const icon2 = document.querySelector('.icon-2')
+const nthChildPosition = 2;
+
+// Add Event Listeners
+icon1.addEventListener('click', switchingClass)
+icon2.addEventListener('click', switchingClass)
+
+// Create Functions
+function switchingClass(e) {
+    // console.log(e.target.parentElement.parentElement.querySelector(`:nth-child(${nthChildPosition})`));
+
+    const grandparent = e.target.parentElement.parentElement;
+    const targetElement = grandparent.firstElementChild.nextElementSibling;
+    console.log(e.target.parentElement.parentElement);
+    console.log(targetElement);
+
+    // e.target.parentElement.parentElement.querySelector(`:nth-child(${nthChildPosition})`).classList.toggle('is-closed');
+    
+    targetElement.classList.toggle('is-closed');
+}
 // Test Function
 // alert("Hello World");
